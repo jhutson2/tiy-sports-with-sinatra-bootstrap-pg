ec9577fcb16b221aa5ec8f1bf0160e364bab59ab
Jonthan Hutson <jhut110766@gmail.com>
2016-11-30 18:10:01 -0500

    Sinatra

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'pg'
 
 get '/' do
+  database = PG.connect(dbname: "tiy-sports")
+  @rows = database.exec("SELECT count(players.id), teams.name, teams.description, teams.image_url FROM players, teams, memberships WHERE players.id = memberships.player_id AND teams.id = memberships.team_id group by teams.name, teams.description, teams,image_url")
 
   erb :home
 end
